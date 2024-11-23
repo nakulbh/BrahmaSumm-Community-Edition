84261606a498c9912bbfd845dbc48f9b66a20f51
nakulbh <nakulbh>
2024-11-23 18:44:21 +0530

    main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import uvicorn
-from summarize import app
+from src.summarize import app
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
